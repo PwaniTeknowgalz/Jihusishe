ce6e2484865daa46c5488d036e1872b581c42b09
Badru254 <suleybadr@yahoo.com>
2022-07-10 11:03:19 +0300

    Fixed County List Header

--- lib/lib/frontend/screens/countieslist.dart
+++ lib/lib/frontend/screens/countieslist.dart
@@ -8,70 +8,127 @@ import 'package:jihusishe/lib/utils/app_constants.dart';
 class CountiesList extends StatelessWidget {
   const CountiesList({Key? key}) : super(key: key);
   static const countylist = [
-    "Mombasa","Kwale","Kilifi","Tana River","Lamu", "Taita Taveta","Garissa","Wajir","Mandera","Marsabit",
-    "Isiolo", "Meru","Tharaka-Nithi","Embu","Kitui","Machakos","Makueni","Nyandarua","Nyeri","Kirinyaga",
-    "Murang/’a","Kiambu","Turkana","West Pokot","Samburu","Trans-Nzoia","Uasin Gishu","Elgeyo-Marakwet","Nandi",
-    "Baringo","Laikipia","Nakuru","Narok","Kajiado","Kericho","Bomet","Kakamega","Vihiga","Bungoma",
-    "Busia","Siaya","Kisumu","Homa Bay","Migori","Kisii","Nyamira","Nairobi",
+    "Mombasa",
+    "Kwale",
+    "Kilifi",
+    "Tana River",
+    "Lamu",
+    "Taita Taveta",
+    "Garissa",
+    "Wajir",
+    "Mandera",
+    "Marsabit",
+    "Isiolo",
+    "Meru",
+    "Tharaka-Nithi",
+    "Embu",
+    "Kitui",
+    "Machakos",
+    "Makueni",
+    "Nyandarua",
+    "Nyeri",
+    "Kirinyaga",
+    "Murang/’a",
+    "Kiambu",
+    "Turkana",
+    "West Pokot",
+    "Samburu",
+    "Trans-Nzoia",
+    "Uasin Gishu",
+    "Elgeyo-Marakwet",
+    "Nandi",
+    "Baringo",
+    "Laikipia",
+    "Nakuru",
+    "Narok",
+    "Kajiado",
+    "Kericho",
+    "Bomet",
+    "Kakamega",
+    "Vihiga",
+    "Bungoma",
+    "Busia",
+    "Siaya",
+    "Kisumu",
+    "Homa Bay",
+    "Migori",
+    "Kisii",
+    "Nyamira",
+    "Nairobi",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(22, 29, 64, 1),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+        ],
+        title: Text(
+          "Select your county",
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: Container(
         height: Get.height,
         width: Get.width,
         child: Column(children: [
           //Header
-          Container(
-            height: 94,
-            width: Get.width,
-            decoration: BoxDecoration(
-              // borderRadius: BorderRadius.only(
-              //     bottomLeft: Radius.circular(30),
-              //     bottomRight: Radius.circular(30)),
-              color: Color.fromRGBO(22, 29, 64, 1),
-            ),
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Select your county",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    
-                    ],
-                  ),
-                ),
-                
-                IconButton(
-                    onPressed: () {
-                    
-                    },
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    )),
-                //SizedBox(width: Get.width * .25),
-              ],
-            ),
-          ),
+          // Container(
+          //   height: 94,
+          //   width: Get.width,
+          //   decoration: BoxDecoration(
+          //     // borderRadius: BorderRadius.only(
+          //     //     bottomLeft: Radius.circular(30),
+          //     //     bottomRight: Radius.circular(30)),
+          //     color: Color.fromRGBO(22, 29, 64, 1),
+          //   ),
+          //   child: Row(
+          //     children: [
+          //       SizedBox(width: 10),
+          //       Expanded(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           children: [
+          //             Text(
+          //               "Select your county",
+          //               style: TextStyle(
+          //                   color: Colors.white,
+          //                   fontSize: 16,
+          //                   fontWeight: FontWeight.bold),
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+
+          //       IconButton(
+          //           onPressed: () {},
+          //           icon: Icon(
+          //             Icons.search,
+          //             color: Colors.white,
+          //           )),
+          //       //SizedBox(width: Get.width * .25),
+          //     ],
+          //   ),
+          // ),
+          
+          
           Expanded(
             child: ListView.separated(
                 separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(thickness: 0.2,
-                color: Color.fromARGB(255, 205, 211, 239)),
-
+                    const Divider(
+                        thickness: 0.2,
+                        color: Color.fromARGB(255, 205, 211, 239)),
                 itemBuilder: (context, index) {
                   return ListTile(
-                    onTap: () => Get.to(()=>Police()),
+                    onTap: () => Get.to(() => Police(county:countylist[index])),
                     title: Text(countylist[index]),
                   );
                 },
