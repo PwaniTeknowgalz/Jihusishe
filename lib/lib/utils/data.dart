bd50dc91550e7cc3e9b5d3002654e6651cf9b78c
Badru254 <suleybadr@yahoo.com>
2022-07-10 11:03:47 +0300

    Added County list police data

---
+++ lib/lib/utils/data.dart
@@ -0,0 +1,171 @@
+class Data{
+
+
+  static  Map policestations = {
+    "Mombasa":[
+
+      //---police station
+      {
+        "name":"Makupa Police Station",
+        "location":"EXM55+65J, Mombasa Road",
+        "phone":"999"
+      },
+      //---end police station
+
+       //---police station
+      {
+        "name":"The Kenya Police - Changamwe Station",
+        "location":"XJFJ+RJ8 Opposite Changamwe Post Office, Magongo Road, Mombasa",
+        "phone":"999"
+      },
+      //---end police station
+
+       //---police station
+      {
+        "name":"Nyali Police Station",
+        "location":"WMXV+27F,Moyne Dr,Mombasa",
+        "phone":"999"
+      },
+      //---end police station
+
+    ],
+    "Kwale":[
+
+    ],
+    "Kilifi":[
+
+    ],
+    "Tana River":[
+
+    ],
+    "Lamu":[
+
+    ],
+    "Taita Taveta":[
+
+    ],
+    "Garissa":[
+
+    ],
+    "Wajir":[
+
+    ],
+    "Mandera":[
+
+    ],
+    "Marsabit":[
+
+    ],
+    "Isiolo":[
+
+    ],
+    "Meru":[
+
+    ],
+    "Tharaka-Nithi":[
+
+    ],
+    "Embu":[
+
+    ],
+    "Kitui":[
+
+    ],
+    "Machakos":[
+
+    ],
+    "Makueni":[
+
+    ],
+    "Nyandarua":[
+
+    ],
+    "Nyeri":[
+
+    ],
+    "Kirinyaga":[
+
+    ],
+    "Murang/’a":[
+
+    ],
+    "Kiambu":[
+
+    ],
+    "Turkana":[
+
+    ],
+    "West Pokot":[
+
+    ],
+    "Samburu":[
+
+    ],
+    "Trans-Nzoia":[
+
+    ],
+    "Uasin Gishu":[
+
+    ],
+    "Elgeyo-Marakwet":[
+
+    ],
+    "Nandi":[
+
+    ],
+    "Baringo":[
+
+    ],
+    "Laikipia":[
+
+    ],
+    "Nakuru":[
+
+    ],
+    "Narok":[
+
+    ],
+    "Kajiado":[
+
+    ],
+    "Kericho":[
+
+    ],
+    "Bomet":[
+
+    ],
+    "Kakamega":[
+
+    ],
+    "Vihiga":[
+
+    ],
+    "Bungoma":[
+
+    ],
+    "Busia":[
+
+    ],
+    "Siaya":[
+
+    ],
+    "Kisumu":[
+
+    ],
+    "Homa Bay":[
+
+    ],
+    "Migori":[
+
+    ],
+    "Kisii":[
+
+    ],
+    "Nyamira":[
+
+    ],
+    "Nairobi":[
+      
+    ],
+  };
+}
